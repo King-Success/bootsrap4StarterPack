93d315253a2e55a864d23f3c44987907b4f505a4
King-Success <code4kings@gmail.com>
2018-06-21 20:15:53 +0100

    modified gulp file to watch changes and serve my project during development

--- gulp.js
+++ gulp.js
@@ -15,4 +15,14 @@ gulp.task('js', function(){
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
         .pipe(gulp.dest("src/js"))
         .pipe(browserSync.stream());
-})
+});
+
+//Watch Sass and Serve
+gulp.task('serve', ['sass'], function(){
+    browserSync.init({
+        server: "./src"
+    });
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
