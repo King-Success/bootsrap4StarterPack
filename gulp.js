4b62c97d5d9e47b56254e7e0a1e9c88d6693b6b4
King-Success <code4kings@gmail.com>
2018-06-21 19:59:57 +0100

    modified gulp file to compile sass and inject in into the browser

--- gulp.js
+++ gulp.js
@@ -2,3 +2,11 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 
+//Compile Sass and inject into Browser
+gulp.task('sass', function(){
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
