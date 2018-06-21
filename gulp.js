55bac649fdf20b62faf69e85d73012354c8b8230
King-Success <code4kings@gmail.com>
2018-06-21 20:09:11 +0100

    modified gulp file to move js file into src

--- gulp.js
+++ gulp.js
@@ -10,3 +10,9 @@ gulp.task('sass', function(){
         .pipe(browserSync.stream());
 });
 
+//Move JS files to src/js
+gulp.task('js', function(){
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(gulp.dest("src/js"))
+        .pipe(browserSync.stream());
+})
